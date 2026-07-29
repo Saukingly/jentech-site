36e9b04a3f9bc6fa3abbd406b48e4715581e3ce5
Saukingly <163466118+Saukingly@users.noreply.github.com>
2026-07-29 10:28:56 -0500

    Fix merge conflict in clientPortal.js

--- server/routes/clientPortal.js
+++ server/routes/clientPortal.js
@@ -130,9 +130,7 @@ router.put('/password', requireClientLogin, async(req, res) => {
     }
 });
 
-<<<<<<< HEAD
+
 module.exports = router;
 
-=======
-module.exports = router;
->>>>>>> 65689711e64d3c199835421fb6806fb0f1d51d26
+
