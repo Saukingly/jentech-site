bf0abdf106a8ff92eb03349d0ea9ac9149f278b5
Saukingly <163466118+Saukingly@users.noreply.github.com>
2026-07-29 10:36:47 -0500

    Fix formatting issues in reports.js

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -126,8 +126,4 @@ router.get('/mine', requireClientLogin, async(req, res) => {
     }
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 65689711e64d3c199835421fb6806fb0f1d51d26
